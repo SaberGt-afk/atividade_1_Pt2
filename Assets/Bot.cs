b0df5afc3b82165ac78f1b334f6ad098b2eb4457
SaberGt-afk <gtsaber0000@gmail.com>
2024-10-21 18:01:16 +0100

    Pursuit

--- Assets/Bot.cs
+++ Assets/Bot.cs
@@ -22,10 +22,26 @@ public class Bot : MonoBehaviour
         Vector3 fleeVector = location - this.transform.position;
         agent.SetDestination(this.transform.position - fleeVector);
     }
+    void Pursue()
+    {
+        Vector3 targetDir = target.transform.position - this.transform.position;
+        float relativeHeading = Vector3.Angle(this.transform.forward, this.transform.TransformVector(target.transform.forward));
+        float toTarget = Vector3.Angle(this.transform.forward, this.transform.TransformVector(targetDir));
+
+        if ((toTarget > 90 && relativeHeading < 20) || target.GetComponent<Drive>().currentSpeed < 0.01f)
+        {
+            Seek(target.transform.position);
+            return;
+        }
+
+
+        float lookAhead = targetDir.magnitude / (agent.speed + target.GetComponent<Drive>().currentSpeed);
+        Seek(target.transform.position + target.transform.forward * lookAhead);
+    }
 
     // Update is called once per frame
     void Update()
     {
-        Flee(target.transform.position);
+        Pursue();
     }
 }
